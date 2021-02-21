c2559a293b16f3a23333485bb5a9cedd8c0ea2d1
王跃光 <wb-wyg523634@antfin.com>
2021-02-21 20:14:22 +0800

    feat: 添加源码包

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('useEffect----->');
+    return () => {
+
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        <p onClick={() => setCount(1)}>Count: {count}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
